d48f632865bc13b05b182348ec4650982a2136b4
vignesh220402 <skvigneshsivakumar@gmail.com>
2025-03-30 13:30:40 +0530

    end-commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/QR-gem/', // Match your repository name
+  base: '/QR-gen/', // Match your repository name
 });
